20001edf27566045dad3fa7da14506351202338c
ken1000minus7 <manjotsinghoberoi.btcse20@pec.edu.in>
2022-10-05 23:36:48 +0530

    i dont like this but i gotta try

--- components/LeaderboardItem.tsx
+++ components/LeaderboardItem.tsx
@@ -8,7 +8,7 @@ export const LeaderboardItem = ({item,rank}: {item : Item,rank: number}) => {
             <div className="text-[30px] mr-[10px]">
                 {rank}
             </div>
-            <img src={item.user?.photoUrl || ""} className="w-[50px] h-[50px] rounded-[50%] mx-[10px]"/>
+            <img src={item.user?.photoUrl || ""} alt="" className="w-[50px] h-[50px] rounded-[50%] mx-[10px]"/>
             <div className="flex-auto text-[25px] mx-[10px] font-bold">
                 {item.user.displayName} <span className="text-[15px] font-normal">{item.user.username}</span>
             </div>
